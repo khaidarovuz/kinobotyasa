178e74f54dedf9cc53efe21c50b74653e24ee242
KHAIDAROV <ulugbekedits@gmail.com>
2026-07-23 12:44:19 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,6 +45,10 @@ class HealthHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"OK")
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.end_headers()
     def log_message(self, *_):
         pass
 
